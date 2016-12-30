819261ae2aad6babdb5cdda7e9be0286e4109dfe
Felipe Jaramillo <pipeluck@gmail.com>
2016-12-30 07:34:44 -0500

    Craete componentes

--- src/app/video-analysis/reading-ease-preview/reading-ease-preview.component.ts
+++ src/app/video-analysis/reading-ease-preview/reading-ease-preview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GaugeSegment, GaugeLabel } from 'ng2-kw-gauge';
 
 @Component({
   selector: 'app-reading-ease-preview',
@@ -12,4 +13,46 @@ export class ReadingEasePreviewComponent implements OnInit {
   ngOnInit() {
   }
 
+
+  colors = {
+    indigo: '#14143e',
+    pink: '#fd1c49',
+    orange: '#ff6e00',
+    yellow: '#f0c800',
+    mint: '#00efab',
+    cyan: '#05d1ff',
+    purple: '#841386',
+    white: '#fff'
+  };
+
+  progressGraph = {
+    bgRadius: 60,
+    bgColor: this.colors.indigo,
+    rounded: false,
+    reverse: false,
+    animationSecs: 1,
+    labels: [
+      new GaugeLabel({
+        color: this.colors.white,
+        text: 'Reading Ease',
+        x: 0,
+        y: 20,
+        fontSize: '1em'
+      }),
+      new GaugeLabel({
+        color: this.colors.pink,
+        text: '81%',
+        x: 0,
+        y: 0,
+        fontSize: '2em'
+      })
+    ],
+    segments: [
+      new GaugeSegment({
+        value: 81,
+        color: this.colors.pink,
+        borderWidth: 20
+      })
+    ]
+  };
 }
