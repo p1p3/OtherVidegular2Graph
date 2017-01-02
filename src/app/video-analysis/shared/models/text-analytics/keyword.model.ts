f6e0b9239a675d77e522e4061696e712b9e4a119
Felipe Jaramillo <pipeluck@gmail.com>
2017-01-02 10:01:59 -0500

    Maps server response to the text analytics

--- src/app/video-analysis/shared/models/text-analytics/keyword.model.ts
+++ src/app/video-analysis/shared/models/text-analytics/keyword.model.ts
@@ -1,6 +1,7 @@
 export class Keyword {
     constructor(public item: string,
         public count: number,
-        public percentage: number) {
+        public percentage: number,
+        public numberOfWords: number) {
     }
 }
--- src/app/video-analysis/shared/services/text-analytics.service.ts
+++ src/app/video-analysis/shared/services/text-analytics.service.ts
@@ -1,3 +1,4 @@
+import { Keyword } from './../models/text-analytics/keyword.model';
 import { Response } from '@angular/http';
 import { IAnalyticsService } from './../../../core/services/def/analytics.service';
 import { TextAnalytics } from './../models/text-analytics/text-analytics.model';
@@ -17,10 +18,54 @@ export class TextAnalyticsService implements ITextAnalyticsService {
     }
 
     private mapResponseToTextAnalytics(response: Response): TextAnalytics {
-        debugger;
-        let textAnalytics: any;
+        let textAnalyticsResponse = JSON.parse(response.json().TextAnalytics) as any;
+        let keyWords = new Array<Keyword>();
+        let oneWord = textAnalyticsResponse.keyword_density['1 word'];
+        let twoWords = textAnalyticsResponse.keyword_density['2 words'];
+        let threeWords = textAnalyticsResponse.keyword_density['3 words'];
+
+        keyWords = keyWords.concat(this.extractKeywords(oneWord, 1));
+        keyWords = keyWords.concat(this.extractKeywords(twoWords, 2));
+        keyWords = keyWords.concat(this.extractKeywords(threeWords, 3));
+
+        let textAnalytics = new TextAnalytics(textAnalyticsResponse.text,
+            textAnalyticsResponse.flesch_kincaid_reading_ease,
+            textAnalyticsResponse.flesch_kincaid_grade_level,
+            textAnalyticsResponse.gunning_fog_score,
+            textAnalyticsResponse.coleman_liau_index,
+            textAnalyticsResponse.smog_index,
+            textAnalyticsResponse.automated_readability_index,
+            textAnalyticsResponse.spache_readability_score,
+            textAnalyticsResponse.dale_chall_readability_score,
+            textAnalyticsResponse.passive_voice_count,
+            textAnalyticsResponse.adverb_count,
+            textAnalyticsResponse.cliche_count,
+            textAnalyticsResponse.long_word_count,
+            textAnalyticsResponse.high_syllable_word_count,
+            textAnalyticsResponse.reading_time,
+            textAnalyticsResponse.speaking_time,
+            textAnalyticsResponse.sentiment,
+            textAnalyticsResponse.longest_word_letters,
+            textAnalyticsResponse.longest_word_syllables,
+            textAnalyticsResponse.longest_sentence_words,
+            keyWords);
 
         return textAnalytics;
+    }
+
+    private mapToKeyword(keywordResponse: any, numberOfWords: number): Keyword {
+        return new Keyword(keywordResponse.item, keywordResponse.count, keywordResponse.percentage, numberOfWords);
+    }
+
+    private extractKeywords(keywordsJson: any, numberOfWords: number): Array<Keyword> {
+        let keyWords = new Array<Keyword>();
+        for (let key in keywordsJson) {
+            if (!keywordsJson.hasOwnProperty(key)) { continue; }
+            let keywordResponse = keywordsJson[key];
+            let keyword = this.mapToKeyword(keywordResponse, numberOfWords);
+            keyWords.push(keyword);
+        }
+        return keyWords;
     }
 
 }
